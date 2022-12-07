8f0580febda24f609c94eafa9b57ef5ee7babc0e
Simon Roschmann <simon.roschmann@tum.de>
2022-12-07 22:19:35 +0100

    CE loss for multiclass single label classification

--- loss.py
+++ loss.py
@@ -1,13 +1,18 @@
 import torch
 import torch.nn as nn
 from torch.nn import BCEWithLogitsLoss
+from torch.nn import functional as F
 
 
 def get_loss(config):
     if config["task"] == "imputation":
         return MaskedMSELoss(reduction="none")
     elif config["task"] == "classification":
-        return BCEWithLogitsLoss(reduction="none")
+        if config.data.multilabel:
+            return BCEWithLogitsLoss(reduction="none")
+        else:  # one class per time series
+            return NoFussCrossEntropyLoss(reduction="none")
+
     else:
         raise ValueError(
             "Loss module for task '{}' does not exist".format(config["task"])
@@ -20,6 +25,22 @@ def l2_reg_loss(model):
     for name, param in model.named_parameters():
         if name == "output_layer.weight":
             return torch.sum(torch.square(param))
+
+
+class NoFussCrossEntropyLoss(nn.CrossEntropyLoss):
+    """
+    pytorch's CrossEntropyLoss is fussy: 1) needs Long (int64) targets only, and 2) only 1D.
+    This function satisfies these requirements
+    """
+
+    def forward(self, inp, target):
+        return F.cross_entropy(
+            inp,
+            target.long(),  # .squeeze(),
+            weight=self.weight,
+            ignore_index=self.ignore_index,
+            reduction=self.reduction,
+        )
 
 
 class MaskedMSELoss(nn.Module):
