e771b5ea9d1cd007c4e8890158a2cb979615ef9b
Simon Roschmann <simon.roschmann@tum.de>
2023-02-08 00:53:09 +0100

    Implement mae classifier

--- models/masked_autoencoder/model.py
+++ models/masked_autoencoder/model.py
@@ -12,16 +12,14 @@
 import torch
 import torch.nn as nn
 from models.patch_tst.layers.pos_encoding import positional_encoding
-from models.patch_tst.model import TSTEncoder
+from models.patch_tst.model import ClassificationPoolHead, TSTEncoder
 
 
 class MaskedAutoencoderTST(nn.Module):
     def __init__(
         self,
         num_patch: int,
-        target_dim: int,
         patch_len=16,
-        stride=16,
         masking_ratio=0.5,
         c_in=3,
         enc_d_model=256,
@@ -33,7 +31,6 @@ class MaskedAutoencoderTST(nn.Module):
         dec_n_layers=8,
         dec_n_heads=8,
         norm_layer=nn.LayerNorm,
-        norm_pix_loss=False,
         shared_embedding=True,
         norm: str = "BatchNorm",
         attn_dropout: float = 0.0,
@@ -44,9 +41,6 @@ class MaskedAutoencoderTST(nn.Module):
         store_attn: bool = False,
         pe: str = "zeros",
         learn_pe: bool = True,
-        head_dropout=0,
-        head_type="prediction",
-        individual=False,
     ):
         super().__init__()
 
@@ -139,11 +133,11 @@ class MaskedAutoencoderTST(nn.Module):
         )  # u: [bs * nvars x num_patch x d_model]
 
         # apply Transformer blocks
-        z = self.encoder(u)
+        z = self.encoder(u, padding_mask)
 
         return z
 
-    def forward_decoder(self, lat, ids_restore):
+    def forward_decoder(self, lat, ids_restore, key_padding_mask):
         # embed tokens
         # for debugging
         x = self.decoder_embed(lat)
@@ -164,7 +158,7 @@ class MaskedAutoencoderTST(nn.Module):
         x = x + self.decoder_pos_embed.to(x.device)
         # apply Transformer blocks
         x = x.squeeze()
-        x = self.decoder(x)
+        x = self.decoder(x, key_padding_mask=key_padding_mask)
         x = self.decoder_norm(x)
 
         # predictor projection
@@ -174,7 +168,117 @@ class MaskedAutoencoderTST(nn.Module):
 
         return x
 
-    def forward(self, imgs, padding_mask, ids_restore):
-        latent = self.forward_encoder(imgs, padding_mask)
-        pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+    def forward(self, imgs, padding_mask, padding_mask_kept, ids_restore):
+        latent = self.forward_encoder(imgs, padding_mask_kept)
+        pred = self.forward_decoder(
+            latent, ids_restore, key_padding_mask=padding_mask
+        )  # [N, L, p*p*3]
+        return pred
+
+
+class MaskedAutoencoderTSTClassifier(nn.Module):
+    def __init__(
+        self,
+        num_patch: int,
+        patch_len=16,
+        masking_ratio=0.5,
+        c_in=3,
+        target_dim=27,
+        enc_d_model=256,
+        enc_d_ff=512,
+        enc_n_layers=8,
+        enc_n_heads=8,
+        norm_layer=nn.LayerNorm,
+        shared_embedding=True,
+        norm: str = "BatchNorm",
+        attn_dropout: float = 0.0,
+        dropout: float = 0.0,
+        act: str = "gelu",
+        res_attention: bool = True,
+        pre_norm: bool = False,
+        store_attn: bool = False,
+        pe: str = "zeros",
+        learn_pe: bool = True,
+        head_dropout=0,
+    ):
+        super().__init__()
+
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.n_vars = c_in
+        self.enc_num_patch = int((1 - masking_ratio) * num_patch)
+        self.patch_len = patch_len
+        self.shared_embedding = shared_embedding
+        self.enc_d_model = enc_d_model
+
+        # Input encoding: projection of feature vectors onto a d-dim vector space
+        if not shared_embedding:
+            self.W_P = nn.ModuleList()
+            for _ in range(self.n_vars):
+                self.W_P.append(nn.Linear(patch_len, enc_d_model))
+        else:
+            self.W_P = nn.Linear(patch_len, enc_d_model)
+
+        # Positional encoding
+        self.encoder_pos_embed = positional_encoding(
+            pe, learn_pe, self.enc_num_patch, enc_d_model
+        )
+
+        self.encoder = TSTEncoder(
+            enc_d_model,
+            enc_n_heads,
+            d_ff=enc_d_ff,
+            n_layers=enc_n_layers,
+            norm=norm,
+            attn_dropout=attn_dropout,
+            dropout=dropout,
+            pre_norm=pre_norm,
+            activation=act,
+            res_attention=res_attention,
+            store_attn=store_attn,
+        )
+        self.encoder_norm = norm_layer(enc_d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.head = ClassificationPoolHead(
+            n_vars=self.n_vars,
+            d_model=enc_d_model,
+            n_classes=target_dim,
+            head_dropout=head_dropout,
+        )
+
+    def forward_encoder(self, x, padding_masks_kept=None):
+        # embed patches
+        bs, num_patch, n_vars, patch_len = x.shape
+        # Input encoding
+        if not self.shared_embedding:
+            x_out = []
+            for i in range(n_vars):
+                z = self.W_P[i](x[:, :, i, :])
+                x_out.append(z)
+            x = torch.stack(x_out, dim=2)
+        else:
+            x = self.W_P(x)  # x: [bs x num_patch x nvars x d_model]
+        x = x.transpose(1, 2)  # x: [bs x nvars x num_patch x d_model]
+
+        u = torch.reshape(
+            x, (bs * n_vars, num_patch, self.enc_d_model)
+        )  # u: [bs * nvars x num_patch x d_model]
+        u = self.dropout(
+            u + self.encoder_pos_embed
+        )  # u: [bs * nvars x num_patch x d_model]
+
+        # apply Transformer blocks
+        z = self.encoder(u)
+
+        return z
+
+    def forward(self, imgs, padding_mask_kept):
+        latent = self.forward_encoder(imgs, padding_mask_kept)
+        # bs x nvars x d_model x num_patch
+        latent = torch.reshape(
+            latent, (imgs.shape[0], imgs.shape[2], imgs.shape[1], -1)
+        ).transpose(2, 3)
+        pred = self.head(latent)
         return pred
