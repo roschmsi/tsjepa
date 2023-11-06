6ac90e02936623328875ffe2ad9b812b0166676e
Simon Roschmann <simon.roschmann@tum.de>
2023-11-06 17:24:02 +0100

    TS2Vec with/out ema with different predictors

--- models/ts2vec/ts2vec.py
+++ models/ts2vec/ts2vec.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from .ema import EMA
 
 
-class TS2Vec(nn.Module):
+class TS2VecEMA(nn.Module):
     """
     Data2Vec main module.
 
@@ -16,40 +16,57 @@ class TS2Vec(nn.Module):
     def __init__(
         self,
         encoder,
+        predictor,
+        predictor_type,
         device,
         average_top_k_layers,
         normalize_targets,
         targets_norm,
+        normalize_pred,
+        pred_norm,
         embed_dim,
         ema_decay,
         ema_end_decay,
         ema_anneal_end_step,
-        skip_embeddings=False,
+        targets_rep,
+        skip_pos_embed,
+        skip_patch_embed,
     ):
-        super(TS2Vec, self).__init__()
+        super(TS2VecEMA, self).__init__()
         self.embed_dim = embed_dim
         self.encoder = encoder
+        self.predictor = predictor
+        self.predictor_type = predictor_type
 
-        if skip_embeddings:
-            skip_keys = ["pos_embed", "patch_embed.weight", "patch_embed.bias"]
+        skip_keys = []
+
+        if skip_pos_embed:
+            skip_keys.append("pos_embed")
+        elif skip_patch_embed:
+            skip_keys.append("patch_embed.weight")
+            skip_keys.append("patch_embed.bias")
         else:
             skip_keys = None
 
+        # ema
         self.ema = EMA(
             self.encoder,
             device=device,
             ema_decay=ema_decay,
             skip_keys=skip_keys,
-        )  # EMA acts as the teacher
-        self.regression_head = nn.Linear(self.embed_dim, self.embed_dim)
-
-        self.average_top_k_layers = average_top_k_layers
-        self.normalize_targets = normalize_targets
-        self.targets_norm = targets_norm
-
+        )
         self.ema_decay = ema_decay
         self.ema_end_decay = ema_end_decay
         self.ema_anneal_end_step = ema_anneal_end_step
+
+        # targets
+        self.average_top_k_layers = average_top_k_layers
+        self.normalize_targets = normalize_targets
+        self.targets_norm = targets_norm
+        self.targets_rep = targets_rep
+
+        self.normalize_pred = normalize_pred
+        self.pred_norm = pred_norm
 
     def ema_step(self):
         """
@@ -69,7 +86,26 @@ class TS2Vec(nn.Module):
         if self.ema.decay < 1:
             self.ema.step(self.encoder)
 
-    def forward(self, X_full, X_masked):
+    def forward_mlp_predictor(self, X_masked):
+        # model forward in online mode (student)
+        X_enc = self.encoder(X_masked)["encoder_out"]  # fetch the last layer outputs
+        y_pred = self.predictor(X_enc)
+
+        return X_enc, y_pred
+
+    def forward_transformer_predictor(self, X_kept, mask, ids_restore):
+        X_enc = self.encoder(X_kept, mask)["encoder_out"]
+        y_pred = self.predictor(X_enc, ids_restore)
+
+        return X_enc, y_pred
+
+    def forward_cnn_predictor(self, X_kept, mask, ids_restore):
+        X_enc = self.encoder(X_kept, mask)["encoder_out"]
+        y_pred = self.predictor(X_enc, ids_restore)
+
+        return X_enc, y_pred
+
+    def forward(self, X_full, X_masked, X_kept, mask, ids_restore):
         """
         Data2Vec forward method.
 
@@ -83,15 +119,150 @@ class TS2Vec(nn.Module):
             Either encoder outputs or a tuple of encoder + EMA outputs
 
         """
-        # model forward in online mode (student)
-        X = self.encoder(X_masked)["encoder_out"]  # fetch the last layer outputs
-        # if X_full is None:
-        #     return X
+        if self.predictor_type in ["mlp", "linear"]:
+            X_enc, y_pred = self.forward_mlp_predictor(X_masked=X_masked)
+        elif self.predictor_type == "transformer":
+            X_enc, y_pred = self.forward_transformer_predictor(
+                X_kept=X_kept, mask=mask, ids_restore=ids_restore
+            )
+        elif self.predictor_type == "cnn":
+            X_enc, y_pred = self.forward_cnn_predictor(
+                X_kept=X_kept, mask=mask, ids_restore=ids_restore
+            )
+        else:
+            raise NotImplementedError
+
+        # if self.normalize_pred:
+        #     if self.pred_norm == "LayerNorm":
+        #         y_pred = F.layer_norm(y_pred.float(), y_pred.shape[-1:])
+        #     elif self.pred_norm == "InstanceNorm":
+        #         y_pred = F.instance_norm(y_pred.float().transpose(1, 2)).transpose(1, 2)
 
         # model forward in offline mode (teacher)
         with torch.no_grad():
             self.ema.model.eval()
-            y = self.ema.model(X_full)["encoder_states"]
+            if self.targets_rep in ["ffn", "block"]:
+                if self.targets_rep == "ffn":
+                    y = self.ema.model(X_full)["encoder_states_ffn"]
+                elif self.targets_rep == "block":
+                    y = self.ema.model(X_full)["encoder_states"]
+                # fetch the last transformer layers outputs
+                y = y[-self.average_top_k_layers :]
+                # take the last k transformer layers
+
+                if self.targets_norm == "LayerNorm":
+                    y = [F.layer_norm(tl.float(), tl.shape[-1:]) for tl in y]
+                    y = sum(y) / len(y)
+                    if self.normalize_targets:
+                        y = F.layer_norm(y.float(), y.shape[-1:])
+                elif self.targets_norm == "InstanceNorm":
+                    y = [
+                        F.instance_norm(tl.float().transpose(1, 2)).transpose(1, 2)
+                        for tl in y
+                    ]
+                    y = sum(y) / len(y)
+                    if self.normalize_targets:
+                        y = F.instance_norm(y.transpose(1, 2)).transpose(1, 2)
+                # elif self.target_norm == "BatchNorm":
+                #     y = [
+                #         F.instance_norm(tl.float().transpose(1, 2)).transpose(1, 2)
+                #         for tl in y
+                #     ]
+                #     y = sum(y) / len(y)
+                #     if self.normalize_targets:
+                #         y = F.instance_norm(y.transpose(1, 2)).transpose(1, 2)
+            elif self.targets_rep == "encoder_out":
+                y = self.ema.model(X_full)["encoder_out"]
+                y = F.layer_norm(y.float(), y.shape[-1:])
+
+        return X_enc, y_pred, y
+
+
+class TS2VecNoEMA(nn.Module):
+    """
+    Data2Vec main module.
+
+    Args:
+         encoder (nn.Module): The encoder module like BEiT, ViT, etc.
+         cfg (omegaconf.DictConfig): The config containing model properties
+    """
+
+    def __init__(
+        self,
+        encoder,
+        predictor,
+        predictor_type,
+        device,
+        average_top_k_layers,
+        normalize_targets,
+        targets_rep,
+        targets_norm,
+        embed_dim,
+    ):
+        super(TS2VecNoEMA, self).__init__()
+        self.embed_dim = embed_dim
+        self.encoder = encoder
+        self.predictor = predictor
+        self.predictor_type = predictor_type
+
+        # targets
+        self.average_top_k_layers = average_top_k_layers
+        self.normalize_targets = normalize_targets
+        self.targets_norm = targets_norm
+        self.targets_rep = targets_rep
+
+    def forward_mlp_predictor(self, X_masked):
+        # model forward in online mode (student)
+        X_enc = self.encoder(X_masked)["encoder_out"]  # fetch the last layer outputs
+        y_pred = self.predictor(X_enc)
+
+        return X_enc, y_pred
+
+    def forward_transformer_predictor(self, X_kept, mask, ids_restore):
+        X_enc = self.encoder(X_kept, mask)["encoder_out"]
+        y_pred = self.predictor(X_enc, ids_restore)
+
+        return X_enc, y_pred
+
+    def forward_cnn_predictor(self, X_kept, mask, ids_restore):
+        X_enc = self.encoder(X_kept, mask)["encoder_out"]
+        y_pred = self.predictor(X_enc, ids_restore)
+
+        return X_enc, y_pred
+
+    def forward(self, X_full, X_masked, X_kept, mask, ids_restore):
+        """
+        Data2Vec forward method.
+
+        Args:
+            src: src tokens (masked inputs for training)
+            trg: trg tokens (unmasked inputs for training but left as `None` otherwise)
+            mask: bool masked indices, Note: if a modality requires the inputs to be masked before forward this param
+            has no effect. (see the Encoder for each modality to see if it uses mask or not)
+
+        Returns:
+            Either encoder outputs or a tuple of encoder + EMA outputs
+
+        """
+        if self.predictor_type in ["mlp", "linear"]:
+            X_enc, y_pred = self.forward_mlp_predictor(X_masked=X_masked)
+        elif self.predictor_type == "transformer":
+            X_enc, y_pred = self.forward_transformer_predictor(
+                X_kept=X_kept, mask=mask, ids_restore=ids_restore
+            )
+        elif self.predictor_type == "cnn":
+            X_enc, y_pred = self.forward_cnn_predictor(
+                X_kept=X_kept, mask=mask, ids_restore=ids_restore
+            )
+        else:
+            raise NotImplementedError
+
+        # model forward in offline mode (teacher)
+        if self.targets_rep in ["ffn", "block"]:
+            if self.targets_rep == "ffn":
+                y = self.encoder(X_full)["encoder_states_ffn"]
+            elif self.targets_rep == "block":
+                y = self.encoder(X_full)["encoder_states"]
             # fetch the last transformer layers outputs
             y = y[-self.average_top_k_layers :]
             # take the last k transformer layers
@@ -109,10 +280,19 @@ class TS2Vec(nn.Module):
                 y = sum(y) / len(y)
                 if self.normalize_targets:
                     y = F.instance_norm(y.transpose(1, 2)).transpose(1, 2)
+            elif self.target_norm == "BatchNorm":
+                y = [
+                    F.instance_norm(tl.float().transpose(1, 2)).transpose(1, 2)
+                    for tl in y
+                ]
+                y = sum(y) / len(y)
+                if self.normalize_targets:
+                    y = F.instance_norm(y.transpose(1, 2)).transpose(1, 2)
+        elif self.targets_rep == "encoder_out":
+            y = self.encoder(X_full)["encoder_out"]
+            y = F.layer_norm(y.float(), y.shape[-1:])
 
-        X = self.regression_head(X)
-
-        return X, y
+        return X_enc, y_pred, y
 
 
 class PredictionHead(nn.Module):
