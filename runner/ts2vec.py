7f1df8df0d93043fa4c8deb1f99ec4762bccd864
Simon Roschmann <simon.roschmann@tum.de>
2023-11-07 14:48:31 +0100

    Plot various std, cov measures

--- runner/ts2vec.py
+++ runner/ts2vec.py
@@ -3,7 +3,7 @@ import logging
 import torch
 from models.ts_jepa.logging import AverageMeter
 from runner.base import BaseRunner
-from models.ts_jepa.vic_reg import vicreg_single_batch
+from models.ts_jepa.vic_reg import vicreg
 import matplotlib.pyplot as plt
 import torch.nn as nn
 from data.dataset import create_patch, random_patch_masking, block_patch_masking
@@ -12,6 +12,7 @@ logger = logging.getLogger("__main__")
 
 
 def plot_cov_matrix(cov_matrix):
+    plt.close()
     fig = plt.figure()
     ax = fig.add_subplot(111)
     cax = ax.matshow(cov_matrix.detach(), vmin=-1, vmax=1)
@@ -19,19 +20,23 @@ def plot_cov_matrix(cov_matrix):
     return fig
 
 
-# TODO integrate revin in encoder ?
 class TS2VecRunner(BaseRunner):
     def __init__(
         self,
         model,
         revin,
         dataloader,
-        device,
         patch_len,
         stride,
         masking,
         masking_ratio,
+        pred_weight,
+        std_weight,
+        cov_weight,
+        device,
         debug,
+        criterion,
+        no_ema=False,
         optimizer=None,
         scheduler=None,
     ):
@@ -54,8 +59,13 @@ class TS2VecRunner(BaseRunner):
         self.epoch_metrics = OrderedDict()
         self.epoch_imgs = OrderedDict()
 
-        self.criterion = nn.SmoothL1Loss(reduction="mean")  # smooth l1
-        self.criterion.to(self.device)
+        self.criterion = criterion
+
+        self.no_ema = no_ema
+
+        self.pred_weight = pred_weight
+        self.std_weight = std_weight
+        self.cov_weight = cov_weight
 
     def train_epoch(self, epoch_num=None):
         self.model.train()
@@ -67,21 +77,24 @@ class TS2VecRunner(BaseRunner):
         loss_cov_meter = AverageMeter()
 
         # track standard deviation
+        enc_std_meter = AverageMeter()
         pred_std_meter = AverageMeter()
         target_std_meter = AverageMeter()
 
         # track covariance
+        enc_cov_meter = AverageMeter()
         pred_cov_meter = AverageMeter()
         target_cov_meter = AverageMeter()
 
         # track covariance
+        enc_cov_matrix = torch.zeros(self.model.embed_dim, self.model.embed_dim)
         pred_cov_matrix = torch.zeros(self.model.embed_dim, self.model.embed_dim)
         target_cov_matrix = torch.zeros(self.model.embed_dim, self.model.embed_dim)
 
         for batch in self.dataloader:
             X, y = batch
 
-            # TODO revin for X and y separately
+            # TODO revin for block masking separately on X and y to reduce the distribution shift
             # X: (bs x seq_len x n_vars)
             if self.revin is not None:
                 X = self.revin(X, "norm")
@@ -115,42 +128,63 @@ class TS2VecRunner(BaseRunner):
             # channel independence
             X = X.squeeze()
             X_masked = X_masked.squeeze()
+            X_kept = X_kept.squeeze()
+            mask = mask.squeeze().bool()
 
-            # squeeze because channel dim is 1
-            pred, target = self.model(
+            X_enc, pred, target = self.model(
                 X_full=X,
                 X_masked=X_masked,
+                X_kept=X_kept,
+                mask=~mask,
+                ids_restore=ids_restore,
             )
 
-            mask = mask.bool()
-
-            # pred = torch.gather(pred, dim=1, index=ids_keep.repeat(1, 1, 128))
-            # target = torch.gather(target, dim=1, index=ids_keep.repeat(1, 1, 128))
-
+            mask = mask.unsqueeze(-1).repeat_interleave(
+                repeats=self.model.embed_dim, dim=-1
+            )
             masked_pred = torch.masked_select(pred, mask)
             masked_target = torch.masked_select(target, mask)
             loss = self.criterion(masked_pred, masked_target)
 
             # only compute vicreg for tracking
             (
+                enc_std_loss,
+                enc_cov_loss,
+                enc_std,
+                enc_cov,
+            ) = vicreg(X_enc)
+            (
                 pred_std_loss,
                 pred_cov_loss,
                 pred_std,
                 pred_cov,
-            ) = vicreg_single_batch(pred)
+            ) = vicreg(pred)
             (
                 target_std_loss,
                 target_cov_loss,
                 target_std,
                 target_cov,
-            ) = vicreg_single_batch(target)
+            ) = vicreg(target)
 
             # TODO we should only track that shit for the masked parts, the others arent interesting
 
+            # TODO discuss, where should vc-reg be applied, for now only on encoder representations
             pred_loss = loss
 
-            std_loss = (pred_std_loss + target_std_loss) / 2
-            cov_loss = (pred_cov_loss + target_cov_loss) / 2
+            if self.no_ema:
+                std_loss = (enc_std_loss + pred_std_loss + target_std_loss) / 3
+                cov_loss = (enc_cov_loss + pred_cov_loss + target_std_loss) / 3
+            else:
+                # TODO currently only optimize encoder representations, however predictor representations could also be optimized
+                std_loss = enc_std_loss
+                cov_loss = enc_cov_loss
+
+            if self.pred_weight > 0:
+                loss = self.pred_weight * loss
+            if self.std_weight > 0:
+                loss += self.std_weight * std_loss
+            if self.cov_weight > 0:
+                loss += self.cov_weight * cov_loss
 
             #  Step 2. Backward & step
             self.optimizer.zero_grad()
@@ -163,17 +197,21 @@ class TS2VecRunner(BaseRunner):
             loss_std_meter.update(std_loss.item(), n=X.shape[0])
             loss_cov_meter.update(cov_loss.item(), n=X.shape[0])
 
+            enc_std_meter.update(enc_std.item(), n=X.shape[0])
             pred_std_meter.update(pred_std.item(), n=X.shape[0])
             target_std_meter.update(target_std.item(), n=X.shape[0])
 
+            enc_cov_meter.update(enc_cov_loss.item(), n=X.shape[0])
             pred_cov_meter.update(pred_cov_loss.item(), n=X.shape[0])
             target_cov_meter.update(target_cov_loss.item(), n=X.shape[0])
 
+            enc_cov_matrix += enc_cov.detach().cpu()
             pred_cov_matrix += pred_cov.detach().cpu()
             target_cov_matrix += target_cov.detach().cpu()
 
             # ema teacher step
-            self.model.ema_step()
+            if not self.no_ema:
+                self.model.ema_step()
 
         # scheduler for learning rate change every epoch
         if self.scheduler is not None:
@@ -187,15 +225,19 @@ class TS2VecRunner(BaseRunner):
         self.epoch_metrics["loss standard deviation"] = loss_std_meter.avg
         self.epoch_metrics["loss covariance"] = loss_cov_meter.avg
 
+        self.epoch_metrics["standard deviation enc"] = enc_std_meter.avg
         self.epoch_metrics["standard deviation pred"] = pred_std_meter.avg
         self.epoch_metrics["standard deviation target"] = target_std_meter.avg
 
+        self.epoch_metrics["covariance enc"] = enc_cov_meter.avg
         self.epoch_metrics["covariance pred"] = pred_cov_meter.avg
         self.epoch_metrics["covariance target"] = target_cov_meter.avg
 
+        enc_cov_matrix = enc_cov_matrix / len(self.dataloader)
         pred_cov_matrix = pred_cov_matrix / len(self.dataloader)
         target_cov_matrix = target_cov_matrix / len(self.dataloader)
 
+        self.epoch_imgs["covariance matrix enc"] = plot_cov_matrix(enc_cov_matrix)
         self.epoch_imgs["covariance matrix pred"] = plot_cov_matrix(pred_cov_matrix)
         self.epoch_imgs["covariance matrix target"] = plot_cov_matrix(target_cov_matrix)
 
@@ -211,14 +253,17 @@ class TS2VecRunner(BaseRunner):
         loss_cov_meter = AverageMeter()
 
         # track standard deviation
+        enc_std_meter = AverageMeter()
         pred_std_meter = AverageMeter()
         target_std_meter = AverageMeter()
 
         # track covariance
+        enc_cov_meter = AverageMeter()
         pred_cov_meter = AverageMeter()
         target_cov_meter = AverageMeter()
 
         # track covariance
+        enc_cov_matrix = torch.zeros(self.model.embed_dim, self.model.embed_dim)
         pred_cov_matrix = torch.zeros(self.model.embed_dim, self.model.embed_dim)
         target_cov_matrix = torch.zeros(self.model.embed_dim, self.model.embed_dim)
 
@@ -255,41 +300,65 @@ class TS2VecRunner(BaseRunner):
             ids_keep = ids_keep.to(self.device)
             ids_restore = ids_restore.to(self.device)
 
-            # squeeze because channel dim is 1
-            pred, target = self.model(
-                X_full=X.squeeze(),
-                X_masked=X_masked.squeeze(),
-                # mask=mask.squeeze(),
+            # channel independence
+            X = X.squeeze()
+            X_masked = X_masked.squeeze()
+            X_kept = X_kept.squeeze()
+            mask = mask.squeeze().bool()
+
+            X_enc, pred, target = self.model(
+                X_full=X,
+                X_masked=X_masked,
+                X_kept=X_kept,
+                mask=~mask,
+                ids_restore=ids_restore,
             )
 
-            mask = mask.bool()
-
-            # pred = torch.gather(pred, dim=1, index=ids_keep.repeat(1, 1, 128))
-            # target = torch.gather(target, dim=1, index=ids_keep.repeat(1, 1, 128))
-
+            mask = mask.unsqueeze(-1).repeat_interleave(
+                repeats=self.model.embed_dim, dim=-1
+            )
             masked_pred = torch.masked_select(pred, mask)
             masked_target = torch.masked_select(target, mask)
             loss = self.criterion(masked_pred, masked_target)
 
             # only compute vicreg for tracking
             (
+                enc_std_loss,
+                enc_cov_loss,
+                enc_std,
+                enc_cov,
+            ) = vicreg(X_enc)
+            (
                 pred_std_loss,
                 pred_cov_loss,
                 pred_std,
                 pred_cov,
-            ) = vicreg_single_batch(pred)
+            ) = vicreg(pred)
             (
                 target_std_loss,
                 target_cov_loss,
                 target_std,
                 target_cov,
-            ) = vicreg_single_batch(target)
+            ) = vicreg(target)
 
             # TODO we should only track that shit for the masked parts, the others arent interesting
 
             pred_loss = loss
-            std_loss = (pred_std_loss + target_std_loss) / 2
-            cov_loss = (pred_cov_loss + target_cov_loss) / 2
+
+            if self.no_ema:
+                std_loss = (enc_std_loss + pred_std_loss + target_std_loss) / 3
+                cov_loss = (enc_cov_loss + pred_cov_loss + target_std_loss) / 3
+            else:
+                # TODO currently only optimize encoder representations, however predictor representations could also be optimized
+                std_loss = enc_std_loss
+                cov_loss = enc_cov_loss
+
+            if self.pred_weight > 0:
+                loss = self.pred_weight * loss
+            if self.std_weight > 0:
+                loss += self.std_weight * std_loss
+            if self.cov_weight > 0:
+                loss += self.cov_weight * cov_loss
 
             # track variance of prediction
             loss_meter.update(loss.item(), n=X.shape[0])
@@ -297,12 +366,15 @@ class TS2VecRunner(BaseRunner):
             loss_std_meter.update(std_loss.item(), n=X.shape[0])
             loss_cov_meter.update(cov_loss.item(), n=X.shape[0])
 
+            enc_std_meter.update(enc_std.item(), n=X.shape[0])
             pred_std_meter.update(pred_std.item(), n=X.shape[0])
             target_std_meter.update(target_std.item(), n=X.shape[0])
 
+            enc_cov_meter.update(enc_cov_loss.item(), n=X.shape[0])
             pred_cov_meter.update(pred_cov_loss.item(), n=X.shape[0])
             target_cov_meter.update(target_cov_loss.item(), n=X.shape[0])
 
+            enc_cov_matrix += enc_cov.detach().cpu()
             pred_cov_matrix += pred_cov.detach().cpu()
             target_cov_matrix += target_cov.detach().cpu()
 
@@ -313,15 +385,19 @@ class TS2VecRunner(BaseRunner):
         self.epoch_metrics["loss standard deviation"] = loss_std_meter.avg
         self.epoch_metrics["loss covariance"] = loss_cov_meter.avg
 
+        self.epoch_metrics["standard deviation enc"] = enc_std_meter.avg
         self.epoch_metrics["standard deviation pred"] = pred_std_meter.avg
         self.epoch_metrics["standard deviation target"] = target_std_meter.avg
 
+        self.epoch_metrics["covariance enc"] = enc_cov_meter.avg
         self.epoch_metrics["covariance pred"] = pred_cov_meter.avg
         self.epoch_metrics["covariance target"] = target_cov_meter.avg
 
+        enc_cov_matrix = enc_cov_matrix / len(self.dataloader)
         pred_cov_matrix = pred_cov_matrix / len(self.dataloader)
         target_cov_matrix = target_cov_matrix / len(self.dataloader)
 
+        self.epoch_imgs["covariance matrix enc"] = plot_cov_matrix(enc_cov_matrix)
         self.epoch_imgs["covariance matrix pred"] = plot_cov_matrix(pred_cov_matrix)
         self.epoch_imgs["covariance matrix target"] = plot_cov_matrix(target_cov_matrix)
 
