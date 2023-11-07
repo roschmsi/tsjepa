866f97f5875a5724dc6273c64be70434014dcb09
Simon Roschmann <simon.roschmann@tum.de>
2023-11-07 14:51:25 +0100

    Refactor tsjepa initialization

--- models/ts_jepa/setup.py
+++ models/ts_jepa/setup.py
@@ -1,8 +1,36 @@
-from functools import partial
 from models.ts_jepa.tensors import trunc_normal_
-from models.ts_jepa.model import TSTClassifier, TSTEncoder, TSTForecaster, TSTPredictor
+from models.ts_jepa.model import (
+    TSTClassifier,
+    TSTEncoder,
+    LinearForecaster,
+    PETForecaster,
+    TSTPredictor,
+)
 import torch
 import torch.nn as nn
+from models.patch_tst.layers.basics import Transpose
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x):
+        return self.norm(x)
+
+
+class BatchNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.norm = nn.Sequential(
+            Transpose(1, 2),
+            nn.BatchNorm1d(dim),
+            Transpose(1, 2),
+        )
+
+    def forward(self, x):
+        return self.norm(x)
 
 
 def init_model_pretraining(
@@ -14,14 +42,22 @@ def init_model_pretraining(
     enc_depth,
     enc_num_heads,
     enc_mlp_ratio,
-    pred_depth,
     pred_embed_dim,
+    pred_depth,
     pred_num_heads,
     pred_mlp_ratio,
+    norm_layer,
+    output_norm,
+    learn_pe,
     drop_rate=0,
     attn_drop_rate=0,
+    drop_path_rate=0,
 ):
-    # TODO choose correct layer norm and activation fn
+    if norm_layer == "BatchNorm":
+        norm_layer = BatchNorm
+    elif norm_layer == "LayerNorm":
+        norm_layer = LayerNorm
+
     encoder = TSTEncoder(
         seq_len=seq_len,
         patch_size=patch_size,
@@ -31,9 +67,12 @@ def init_model_pretraining(
         num_heads=enc_num_heads,
         mlp_ratio=enc_mlp_ratio,
         qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        norm_layer=norm_layer,
         drop_rate=drop_rate,
         attn_drop_rate=attn_drop_rate,
+        drop_path_rate=drop_path_rate,
+        output_norm=output_norm,
+        learn_pe=learn_pe,
     )
     predictor = TSTPredictor(
         num_patches=encoder.patch_embed.num_patches,
@@ -43,9 +82,12 @@ def init_model_pretraining(
         num_heads=pred_num_heads,
         mlp_ratio=pred_mlp_ratio,
         qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        norm_layer=norm_layer,
         drop_rate=drop_rate,
         attn_drop_rate=attn_drop_rate,
+        drop_path_rate=drop_path_rate,
+        output_norm=output_norm,
+        learn_pe=learn_pe,
     )
 
     def init_weights(m):
@@ -77,11 +119,20 @@ def init_classifier(
     enc_depth,
     enc_num_heads,
     enc_mlp_ratio,
+    norm_layer,
     n_classes,
     head_dropout,
+    learn_pe,
+    output_norm,
     drop_rate=0,
     attn_drop_rate=0,
+    drop_path_rate=0,
 ):
+    if norm_layer == "BatchNorm":
+        norm_layer = BatchNorm
+    elif norm_layer == "LayerNorm":
+        norm_layer = LayerNorm
+
     classifier = TSTClassifier(
         seq_len=seq_len,
         patch_size=patch_size,
@@ -90,10 +141,14 @@ def init_classifier(
         enc_depth=enc_depth,
         enc_num_heads=enc_num_heads,
         enc_mlp_ratio=enc_mlp_ratio,
+        norm_layer=norm_layer,
         drop_rate=drop_rate,
         attn_drop_rate=attn_drop_rate,
+        drop_path_rate=drop_path_rate,
         n_classes=n_classes,
         head_dropout=head_dropout,
+        learn_pe=learn_pe,
+        output_norm=output_norm,
     )
 
     def init_weights(m):
@@ -121,26 +176,57 @@ def init_forecaster(
     enc_depth,
     enc_num_heads,
     enc_mlp_ratio,
+    norm_layer,
     head_dropout,
     num_patch,
     forecast_len,
+    learn_pe,
+    head,
     drop_rate=0,
     attn_drop_rate=0,
+    drop_path_rate=0,
 ):
-    forecaster = TSTForecaster(
-        seq_len=seq_len,
-        patch_size=patch_size,
-        in_chans=in_chans,
-        enc_embed_dim=enc_embed_dim,
-        enc_depth=enc_depth,
-        enc_num_heads=enc_num_heads,
-        enc_mlp_ratio=enc_mlp_ratio,
-        drop_rate=drop_rate,
-        attn_drop_rate=attn_drop_rate,
-        head_dropout=head_dropout,
-        num_patch=num_patch,
-        forecast_len=forecast_len,
-    )
+    if norm_layer == "BatchNorm":
+        norm_layer = BatchNorm
+    elif norm_layer == "LayerNorm":
+        norm_layer = LayerNorm
+
+    if head == "linear":
+        forecaster = LinearForecaster(
+            seq_len=seq_len,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            enc_embed_dim=enc_embed_dim,
+            enc_depth=enc_depth,
+            enc_num_heads=enc_num_heads,
+            enc_mlp_ratio=enc_mlp_ratio,
+            norm_layer=norm_layer,
+            drop_rate=drop_rate,
+            attn_drop_rate=attn_drop_rate,
+            drop_path_rate=drop_path_rate,
+            head_dropout=head_dropout,
+            num_patch=num_patch,
+            forecast_len=forecast_len,
+            learn_pe=learn_pe,
+        )
+    elif head == "transformer":
+        forecaster = PETForecaster(
+            seq_len=seq_len,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            enc_embed_dim=enc_embed_dim,
+            enc_depth=enc_depth,
+            enc_num_heads=enc_num_heads,
+            enc_mlp_ratio=enc_mlp_ratio,
+            norm_layer=norm_layer,
+            drop_rate=drop_rate,
+            attn_drop_rate=attn_drop_rate,
+            drop_path_rate=drop_path_rate,
+            head_dropout=head_dropout,
+            num_patch=num_patch,
+            forecast_len=forecast_len,
+            learn_pe=learn_pe,
+        )
 
     def init_weights(m):
         if isinstance(m, torch.nn.Linear):
@@ -158,22 +244,8 @@ def init_forecaster(
     return forecaster
 
 
-def init_opt(
-    encoder,
-    predictor,
-    lr,
-    weight_decay,
-    # iterations_per_epoch,
-    # start_lr,
-    # ref_lr,
-    # warmup,
-    # num_epochs,
-    # wd=1e-6,
-    # final_wd=1e-6,
-    # final_lr=0.0,
-    # use_bfloat16=False,
-    # ipe_scale=1.25,
-):
+# exclude bias and 1d weights (normalization) from weight decay
+def init_optimizer_enc_pred(encoder, predictor, lr, weight_decay, epochs):
     param_groups = [
         {
             "params": (
@@ -209,21 +281,66 @@ def init_opt(
         },
     ]
 
-    # TODO consider learning rate
-    optimizer = torch.optim.AdamW(param_groups, lr=lr, weight_decay=weight_decay)
-    # scheduler = WarmupCosineSchedule(
-    #     optimizer,
-    #     warmup_steps=int(warmup * iterations_per_epoch),
-    #     start_lr=start_lr,
-    #     ref_lr=ref_lr,
-    #     final_lr=final_lr,
-    #     T_max=int(ipe_scale * num_epochs * iterations_per_epoch),
-    # )
-    # wd_scheduler = CosineWDSchedule(
-    #     optimizer,
-    #     ref_wd=wd,
-    #     final_wd=final_wd,
-    #     T_max=int(ipe_scale * num_epochs * iterations_per_epoch),
-    # )
-    # scaler = torch.cuda.amp.GradScaler() if use_bfloat16 else None
+    # param_groups = [*encoder.parameters(), *predictor.parameters()]
+
+    optimizer = torch.optim.AdamW(
+        param_groups,
+        lr=lr,
+        weight_decay=weight_decay,
+    )
+
     return optimizer
+
+
+def init_optimizer(model, lr, weight_decay, epochs):
+    param_groups = [
+        {
+            "params": (
+                p
+                for n, p in model.named_parameters()
+                if ("bias" not in n) and (len(p.shape) != 1)
+            )
+        },
+        {
+            "params": (
+                p
+                for n, p in model.named_parameters()
+                if ("bias" in n) or (len(p.shape) == 1)
+            ),
+            "WD_exclude": True,
+            "weight_decay": 0,
+        },
+    ]
+
+    optimizer = torch.optim.AdamW(
+        param_groups,
+        lr=lr,
+        weight_decay=weight_decay,
+    )
+
+    return optimizer
+
+
+def init_scheduler(optimizer, config):
+    if config.scheduler == "CosineAnnealingLR":
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer=optimizer, T_max=config.epochs
+        )
+    elif config.scheduler == "CosineAnnealingLRWithWarmup":
+        scheduler_1 = torch.optim.lr_scheduler.LinearLR(
+            optimizer=optimizer,
+            start_factor=config.start_factor,
+            total_iters=config.warmup,
+        )
+        scheduler_2 = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer=optimizer, T_max=config.epochs - config.warmup
+        )
+        scheduler = torch.optim.lr_scheduler.SequentialLR(
+            optimizer,
+            schedulers=[scheduler_1, scheduler_2],
+            milestones=[config.warmup],
+        )
+    else:
+        scheduler = None
+
+    return scheduler
